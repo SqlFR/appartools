eb4aaf5667fc471f2107d07edcd1fda387959a77
SqlFR <florian.lazzarini@gmail.com>
2024-06-03 07:51:17 -0400

    change name folder primary

--- apartools/asgi.py
+++ apartools/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for aparttools project.
+ASGI config for apartools project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aparttools.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apartools.settings')
 
 application = get_asgi_application()
--- apartools/urls.py
+++ apartools/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for aparttools project.
+URL configuration for apartools project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
