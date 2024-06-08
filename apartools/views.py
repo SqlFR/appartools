941f7c6e684d207e33d57588278d9b5c45fafced
SqlFR <florian.lazzarini@gmail.com>
2024-06-08 16:51:20 -0400

    frontend index terminé

--- apartools/views.py
+++ apartools/views.py
@@ -3,18 +3,22 @@ from django.apps import apps
 
 
 def index(request):
-    # Récuère toutes les apps
-    list_apps = apps.get_app_configs()
-    apps_default = [
-        'Administration', 'Authentification et autorisation',
-        'Types de contenus', 'Sessions', 'Messages', 'Fichiers statiques', 'Liste d\'outils'
-    ]
-    # Les ajoute en excluant celles par défault
-    app_names = [app.verbose_name for app in list_apps if app.verbose_name not in apps_default]
-    app_urls = [app.url for app in list_apps if app.verbose_name not in apps_default]
+    # Récuère l'app
+    app = apps.get_app_config('check_arrangement')
+    # apps_default = [
+    #     'Administration', 'Authentification et autorisation',
+    #     'Types de contenus', 'Sessions', 'Messages', 'Fichiers statiques', 'Liste d\'outils'
+    # ]
+    # # Les ajoute en excluant celles par défault
+    # app_names = [app.verbose_name for app in list_apps if app.verbose_name not in apps_default]
+    # app_urls = [app.url for app in list_apps if app.verbose_name not in apps_default]
+    # app_names = [app.verbose_name for app in list_apps]
+    # app_urls = [app.url for app in list_apps]
 
     context = {
-        'app_names': app_names,
-        'app_urls': app_urls
+        'app_name': app.verbose_name,
+        'app_url': app.url,
+        'app_description': app.description,
+        'app_incoming': app.incoming,
     }
     return render(request, 'index.html', context)
