e7ef179aea5c70c6a87a14c05f9e353aada2cc8e
SqlFR <florian.lazzarini@gmail.com>
2024-06-08 08:47:31 -0400

    ajout app apartools ds app.py

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./check_arrangement/**/*.{html,js}"
+      "./check_arrangement/**/*.{html,js}",
+      "./apartools/**/*.{html,js}"
   ],
   theme: {
     extend: {},
