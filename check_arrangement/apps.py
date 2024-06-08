91ab1d31f6730b40bc609acf2f4481d220666c3a
SqlFR <florian.lazzarini@gmail.com>
2024-06-08 16:51:20 -0400

    frontend index terminé

--- check_arrangement/apps.py
+++ check_arrangement/apps.py
@@ -5,4 +5,6 @@ class CheckArrangementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'check_arrangement'
     verbose_name = 'Agencement d\'appart'
+    description = "Renseigner les incidents sur l'appart : meubles manquants, défauts de construction ou autres."
+    incoming = "Permettra le suivit de l'avancement général de l'appart."
     url = 'check-apartment/'
