9c33b04f92a223d40025aea0a30e9e72c30032da
SqlFR <florian.lazzarini@gmail.com>
2024-06-09 16:04:27 -0400

    modal finish, start form add_apart

--- check_arrangement/static/check_arrangement/js/modal.js
+++ check_arrangement/static/check_arrangement/js/modal.js
@@ -1,20 +1,16 @@
-// script.js
+// modal delete apart
 document.addEventListener('DOMContentLoaded', (event) => {
     const modal = document.getElementById("deleteModal");
-    const span = document.getElementsByClassName("close")[0];
     const confirmDeleteBtn = document.getElementById("confirmDelete");
+    const span = document.getElementsByClassName("close-delete")[0];
     let deleteUrl = '';
 
-    document.querySelectorAll('.delete-btn').forEach(button => {
+    document.querySelectorAll('.btn-delete').forEach(button => {
         button.addEventListener('click', () => {
             deleteUrl = `/check-apartment/delete_apartment/${button.dataset.id}`;
             modal.style.display = "block";
         });
     });
-
-    span.onclick = () => {
-        modal.style.display = "none";
-    };
 
     confirmDeleteBtn.onclick = () => {
         window.location.href = deleteUrl;
@@ -24,9 +20,42 @@ document.addEventListener('DOMContentLoaded', (event) => {
         modal.style.display = "none";
     };
 
+    span.onclick = () => {
+        modal.style.display = "none"
+    };
+
     window.onclick = (event) => {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     };
+});
+
+
+// modal add apart
+document.addEventListener('DOMContentLoaded',(event) => {
+    const modal = document.getElementById('addModal');
+    const confirmAddBtn = document.getElementById('confirmAdd');
+    const span = document.getElementsByClassName("close-add")[0];
+    const btnAdd = document.getElementById('btn-add')
+    let addUrl = ''
+
+    btnAdd.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+
+    confirmAddBtn.onclick = () => {
+        addUrl = '/check-apartment/add_apartment/'
+        window.location.href = addUrl
+    };
+
+    span.onclick = () => {
+        modal.style.display = "none"
+    };
+
+    window.onclick = (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+};
 });
