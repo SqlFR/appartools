95111789c1c672591c8f805b0ad5c514b44f0c56
SqlFR <florian.lazzarini@gmail.com>
2024-06-13 08:40:44 -0400

    front add_issue finish, reagencement page add_issue et results

--- check_arrangement/urls.py
+++ check_arrangement/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('add_apartment/', views.add_apartment, name='add_apartment'),
     path('delete_apartment/<int:apartment_id>', views.delete_apartment, name='delete_apartment'),
     path('delete_issue/<int:apartmentissues_id>/', views.delete_issue, name='delete_issue'),
-    path('<int:apartment_id>/', views.detail, name="detail"),
+    path('<int:apartment_id>/', views.add_issue, name="add_issue"),
     path('<int:apartment_id>/results/', views.results, name="results"),
 ]
