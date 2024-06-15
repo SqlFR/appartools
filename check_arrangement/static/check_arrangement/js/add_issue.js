cdb16297de5c2417fa62268796eda5c4039ab2af
SqlFR <florian.lazzarini@gmail.com>
2024-06-15 13:56:52 -0400

    modal add_issue.html ajoutée

---
+++ check_arrangement/static/check_arrangement/js/add_issue.js
@@ -0,0 +1,19 @@
+// Ajoute class label-input a la div qui contient label/input ds le form add issue
+const formDivs = document.querySelectorAll('.form-add > div');
+formDivs.forEach((div) => {
+    div.classList.add('label-input')
+})
+
+// Gestion de la modal lors de l'ajout d'incident
+const modalSuccessIssue = document.getElementById('add-issue-modal-success');
+document.addEventListener("DOMContentLoaded", function() {
+    // Récupère le paramètre dans l'URL
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('success')) {
+        modalSuccessIssue.classList.add('show');
+        setInterval(function() {
+            modalSuccessIssue.classList.remove('show');
+        }, 2000)
+
+    }
+})
