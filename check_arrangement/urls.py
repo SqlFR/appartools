c5297b8ab19116c180f8b8975d0da41f08b9849d
SqlFR <florian.lazzarini@gmail.com>
2024-06-23 18:39:53 -0400

    Checklist fonctionnel avec boutons

--- check_arrangement/urls.py
+++ check_arrangement/urls.py
@@ -10,7 +10,10 @@ urlpatterns = [
     path('delete_issue/<int:apartmentissues_id>/', views.delete_issue, name='delete_issue'),
     path('add_issue/<int:apartment_id>/', views.add_issue, name="add_issue"),
     path('sheets/<int:apartment_id>/', views.sheets, name='sheets'),
-    path('sheets/update/<int:apartment_id>/', views.update, name='update'),
+    path('sheets/to_delivery/<int:apartment_id>/', views.to_delivery, name='to_delivery'),
+    path('sheets/delivery/<int:sheet_id>/', views.delivery, name='delivery'),
+    path('sheets/handled/<int:sheet_id>/', views.handled, name='handled'),
+    path('sheets/unavailable/<int:sheet_id>/', views.unavailable, name='unavailable'),
     path('sheets/update_to_not_handled/<int:apartment_sheet_handled_id>/', views.update_to_not_handled, name='update_to_not_handled'),
     path('<int:apartment_id>/results/', views.results, name="results"),
 ]
